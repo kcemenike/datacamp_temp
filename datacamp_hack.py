7ab161877afdfd0f6e231c57d5c4012023ad905b
kcemenike <29731161+kcemenike@users.noreply.github.com>
2019-07-05 23:21:02 +0100

    Create datacamp_hack.py

---
+++ datacamp_hack.py
@@ -0,0 +1,9 @@
+#Get Data from DataCamp #DataCampHack
+
+!git init
+!git add .
+!git config --global user.email 'kcemenike@live.com'
+!git config --global user.name 'KC Emenike'
+!git commit -m 'first try'
+!git remote add origin https://kcemenike:{password}@github.com/kcemenike/datacamp_temp.git
+!git push -u origin master
